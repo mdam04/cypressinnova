d903ee037f084aec176be226eeb00088fe537a7c
mm <madhangovindd@gmail.com>
2025-05-22 10:39:13 +0530

    Fix Next.js configuration issues

---
+++ next.config.mjs
@@ -0,0 +1,33 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Prevent 'async_hooks' from being bundled on the client
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        async_hooks: false,
+      };
+    }
+    return config;
+  },
+};
+
+export default nextConfig;
