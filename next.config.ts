d7b174773a7e3fc3ee79d8d044cd4b5a9d16cfee
mm <madhangovindd@gmail.com>
2025-05-26 09:19:33 +0530

    Updated next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -20,7 +20,8 @@ const nextConfig: NextConfig = {
   },
   experimental: {
     clientRouterFilter: false,
-    clientRouterFilterRedirects: false
+    clientRouterFilterRedirects: false,
+    serverActions: true
   }
 };
 
